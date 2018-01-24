f2530b01ba0d3ca4ceb697e08ef803a0cf66a7f6
Shriraj <shri.93.raj@gmail.com>
2018-01-24 21:58:56 +0530

    Add layout

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Layout />
         <PokeList listOfPokemon={this.state.pokemon} />
       </div>
     );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import '../styles/PokeCard.css'
 
 const layout = () => {
 
         return(
-                <div style={{height: '300px', position: 'relative'}}>
-                <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-                <Header transparent title="ABC" style={{color: 'white'}}>
+                <div style={{height: '200px', position: 'background'}}>
+                <Layout style={{background: 'url(https://wallpapercave.com/wp/JKJhFUk.jpg) center / cover'}}>
+                <Header transparent title="Pokedex" style={{color: 'white'}}>
                 <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
                 </Navigation>
                 </Header>
                 <Drawer title="Title">
                 <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+                <a href="#">Dummy</a>
                         </Navigation>
                         </Drawer>
                         <Content />
