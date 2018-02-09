d8da10fb3336598f753450096ef6e7de0ce3efbb
Shriraj <shri.93.raj@gmail.com>
2018-02-10 01:19:39 +0530

    Added dependencies

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -8,9 +8,7 @@ const PokeList = ({listOfPokemon, openModal, pokemonStartIndex}) => {
     let pokemon = listOfPokemon.map((creature, index) =>{
         const imgBaseUri = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; 
         let pokemonId = index + pokemonStartIndex;
-        console.log(pokemonId);
         let imageFinalUri = imgBaseUri + pokemonId + '.png';
-        console.log(imageFinalUri);
         return(
 
             <div key={creature.name} >
